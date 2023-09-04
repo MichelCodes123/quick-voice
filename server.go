0a23f23837f21f645ba6a97b75ad4a1eeb863b17
Mich <interstellar.acc@gmail.com>
2023-09-04 11:58:06 -0400

    Basic Server routing

--- server.go
+++ server.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("This is, the voice")
+	//Registering route handlers
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/",fs)
+
+	//Sets up port for listening
+	fmt.Println("Server Started")
+	log.Fatal(http.ListenAndServe("localhost:3000", nil))
 }
