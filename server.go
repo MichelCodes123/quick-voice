4af632cc181777745c0ef5753f650e5147c3f6c7
Mich <interstellar.acc@gmail.com>
2023-09-04 20:14:27 -0400

    V1 setup of templating

--- server.go
+++ server.go
@@ -9,8 +9,7 @@ import (
 
 var templates = template.Must(template.ParseFiles("templates/index.html", "templates/analytics.html"))
 
-func renderTemplate(w http.ResponseWriter, dir string) {
-
+func renderTemplate(w http.ResponseWriter, dir string, data any) {
 	err := templates.ExecuteTemplate(w, dir, nil)
 	//Handling errors associated with ParseFiles method
 	if err != nil {
@@ -21,18 +20,25 @@ func renderTemplate(w http.ResponseWriter, dir string) {
 
 func main() {
 	//Registering route handlers
-	//fs := http.FileServer(http.Dir("static/"))
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	//Setup as the root directory for the app, rejects any request that is not defined in the server
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Println(r.URL.Path)
-		renderTemplate(w, "index.html")
+		if r.URL.Path != "/" {
+			http.Error(w, "404 not found", http.StatusInternalServerError)
+			return
+		}
+		renderTemplate(w, "index.html", nil)
 	})
 
 	//http package provides the HandleFunc method, which accepts a path and a handler containing response and request information.
 	http.HandleFunc("/analytics", func(w http.ResponseWriter, r *http.Request) {
-		renderTemplate(w, "analytics.html")
+		renderTemplate(w, "analytics.html", nil)
 	})
 
 	//Sets up port for listening
-	fmt.Println("Server Starteds")
+	fmt.Println("Server Started")
 	log.Fatal(http.ListenAndServe("127.0.0.1:3000", nil))
 }
