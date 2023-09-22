1512e7aed99982c8f0f3dcaf8a84fd6fdd028fa7
Mich <interstellar.acc@gmail.com>
2023-09-22 10:57:59 -0400

    Developing more invoice fields

--- static/JS/index.js
+++ static/JS/index.js
@@ -1,18 +1,21 @@
 
-const newItem = document.getElementById("summary-add")
 const itemTable = document.getElementById("summary-table-body")
 const data = document.querySelector(".summary-data").cloneNode(true)
 const emptyPreset = document.querySelector(".no-preset");
 const filledPreset = document.querySelector(".preset-ready");
 const presetSection = document.querySelector(".id-presets")
+let subtotal = document.getElementById("summary-sub")
+let tax = document.getElementById("summary-tax")
+let ship = document.getElementById("summary-ship")
+let total= document.getElementById("summary-total")
+
+
 
 const presetSelect = document.getElementById("id-presets-list")
-
 presetSelect.addEventListener("change", selectHandler)
-
 function selectHandler(e) {
 
-    presetSection.querySelectorAll(".preset").forEach(x =>{
+    presetSection.querySelectorAll(".preset").forEach(x => {
         if (!x.classList.contains("hide-preset")) {
             x.classList.toggle("hide-preset")
         }
@@ -24,6 +27,7 @@ document.querySelectorAll(".summary-calc").forEach(x => {
     x.addEventListener("change", changeHandler);
 })
 
+const newItem = document.getElementById("summary-add")
 newItem.addEventListener("click", clickHandler)
 
 function clickHandler(e) {
@@ -43,8 +47,23 @@ function changeHandler(e) {
     let qty = row.querySelector(".qty").value;
     let cpu = row.querySelector(".cpu").value;
 
-    row.querySelector(".total").value = qty * cpu;
+    row.querySelector(".total").value = (qty * cpu).toFixed(2);
+    summaryHandler()
 
+}
+
+tax.addEventListener("change", summaryHandler);
+ship.addEventListener("change",summaryHandler); 
+
+function summaryHandler(){
+
+    let sub = 0;
+    document.querySelectorAll(".total").forEach(x => {
+        sub += parseFloat(x.value);
+
+    })
+    subtotal.value = sub.toFixed(2)
+    total.innerText = `Total: ${parseFloat(sub + parseFloat(sub*tax.value) + parseFloat(ship.value)).toFixed(2)}`
 }
 
 document.addEventListener("DOMContentLoaded", loadHandler)
@@ -53,7 +72,6 @@ function loadHandler() {
 
     fetch("\presets")
         .then(response => {
-            console.log(...response.headers)
             return response.json()
         })
         .then(x => {
@@ -79,7 +97,6 @@ function loadHandler() {
                     presetSection.appendChild(preset);
                     preset.classList.add(`p${i}`)
                     preset.classList.add(`preset`)
-                    console.log(preset)
                 }
             }
             console.log(x)
